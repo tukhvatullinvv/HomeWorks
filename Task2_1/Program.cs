a37153d6a4779c43ff5bd42e7c5a414a63385a87
Вахит Тухватуллин <vahta98@gmail.com>
2025-03-01 00:36:04 +0300

    Таск 2.1

---
+++ Task2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int t = b;
+            b = a;
+            a = c;
+            c = t;
+            Console.WriteLine("a={0}, b={1},c={2}", a, b, c);
+            Console.ReadKey();
+        }
+    }
+}
