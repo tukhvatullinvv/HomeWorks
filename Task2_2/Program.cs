de5dc9bae9f8d5dc33c104a64fb5b7c1771e1def
Вахит Тухватуллин <vahta98@gmail.com>
2025-03-05 00:09:18 +0300

    Такс 2.2

---
+++ Task2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите угол в градусах, минутах, секундах");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            double d = ((a - (a / 360) * 360) + b / 60.0 + (c / 360.0)) *Math.PI / 180.0;
+            Console.WriteLine("Угол в радианах={0}", d);
+            Console.ReadKey();
+        }
+    }
+}
