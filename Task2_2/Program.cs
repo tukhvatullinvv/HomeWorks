7624807978215b1a45112a40a501474051688244
Вахит Тухватуллин <vahta98@gmail.com>
2025-03-10 23:16:55 +0300

    Таск 2.2

--- Task2_2/Program.cs
+++ Task2_2/Program.cs
@@ -8,9 +8,8 @@
             int a = Convert.ToInt32(Console.ReadLine());
             int b = Convert.ToInt32(Console.ReadLine());
             int c = Convert.ToInt32(Console.ReadLine());
-            int e = (a * 3600 + b * 60 + c) / 3600/360*360;
             double d = ((a + b / 60.0 + c / 3600.0) - ((a * 3600 + b * 60 + c) / 3600)/360*360) * Math.PI / 180.0;
-            Console.WriteLine("Угол в радианах={0},{1}" ,d, e);
+            Console.WriteLine("Угол в радианах={0}" ,d);
             Console.ReadKey();
         }
     }
