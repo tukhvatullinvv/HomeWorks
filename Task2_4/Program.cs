d7c8d6fdb08bbd73c4e7710ac06b0a8303aa072e
Вахит Тухватуллин <vahta98@gmail.com>
2025-03-11 17:38:16 +0300

    Таск 2.4

---
+++ Task2_4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 4х-значное число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = a / 1000;
+            int c = (a % 1000) / 100;
+            int d = (a % 100) / 10;
+            int e = a % 10;
+            Console.WriteLine("{0}{3}{2}{1}", b, c, d, e);
+            Console.ReadKey();
+
+        }
+    }
+}
