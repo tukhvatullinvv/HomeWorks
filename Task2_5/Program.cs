34161c0a9926838c11cf5bfe8e72716c94b35f45
Вахит Тухватуллин <vahta98@gmail.com>
2025-03-12 00:37:20 +0300

    Таск 2.5

---
+++ Task2_5/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task2_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            a = Math.Ceiling(a);
+            Console.WriteLine("{0}", a);
+            Console.ReadKey();
+        }
+    }
+}
