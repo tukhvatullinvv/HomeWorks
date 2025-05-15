237ce0224962d3892b23ab7047ad3c49030c45ac
Вахит Тухватуллин <vahta98@gmail.com>
2025-05-15 17:34:10 +0300

    Таск 4.5

---
+++ Task4_4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a < 0)
+                while (a < 0)
+                {
+                    Console.WriteLine("a должно быть больше 0");
+                    a = Convert.ToInt32(Console.ReadLine());
+                }
+            Console.WriteLine("Введите число b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (b > 0)
+                while (b > 0)
+                {
+                    Console.WriteLine("b должно быть меньше 0");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                }
+            double c = 1;
+            for (int i = 1; i <= (b * (-1)); i++)
+            {
+                c = c * a;
+            }
+            Console.WriteLine("{0}", 1 / c);
+            Console.ReadKey();
+        }
+    }
+}
