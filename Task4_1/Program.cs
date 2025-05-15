dd1a8ce1d5985d367700036b19a737cfd17f0b76
Вахит Тухватуллин <vahta98@gmail.com>
2025-05-15 10:43:40 +0300

    Таск 4.1

---
+++ Task4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int k = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                k = k * i;
+            }
+            Console.WriteLine("{0}", k);
+            Console.ReadKey();
+        }
+    }
+}
