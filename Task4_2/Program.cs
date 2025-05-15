95670d6248af8455f8105f4b53c8e5339a1e18af
Вахит Тухватуллин <vahta98@gmail.com>
2025-05-15 11:29:22 +0300

    Таск 4.2

---
+++ Task4_2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            double k = 1;
+            for (double i = 2; i <= a; i++)
+            {
+                k = k + (1 / i);
+            }
+            Console.WriteLine("{0}", k);
+            Console.ReadKey();
+        }
+    }
+}
