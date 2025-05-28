6d429036cfd3d61e817ba5a0521abf8f379eb74a
Вахит Тухватуллин <vahta98@gmail.com>
2025-05-28 09:43:17 +0300

    Таск 5.1

--- Task5_1/Program.cs
+++ Task5_1/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int n = 10;
+            int[] mass = new int[n];
+            Random rnd = new Random();
+            int countEven = 0;
+            for (int i = 0; i < n; i++)
+            {
+                mass[i] = rnd.Next(0, 101);
+                Console.WriteLine("{0}", mass[i]);
+                if (mass[i] % 2 == 0)
+                { countEven++; }
+            }
+            if ((n / countEven) < 2)
+            {
+                Console.WriteLine("Четных больше");
+            }
+            else
+            {
+                if ((n / countEven) == 2)
+                    Console.WriteLine("Четных и нечетных поровну");
+                else
+                {
+                    Console.WriteLine("Нечетных больше");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
