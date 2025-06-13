a1281a11fa2f0a455299849aa84586a7a7f4fa94
Вахит Тухватуллин <vahta98@gmail.com>
2025-06-13 21:44:09 +0300

    Таск6.2

---
+++ Task6_2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите преложение без знаков препинания: ");
+            string sentence = Console.ReadLine().ToLower().Replace(" ", "");
+            int l = sentence.Length;
+            bool f = true;
+            for (int i = 0; i < l; i++)
+            {
+                if (sentence[i] != sentence[l - i - 1])
+                {
+                    f = false;
+                    break;
+                }
+            }
+            if (f)
+            {
+                Console.WriteLine("Является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
