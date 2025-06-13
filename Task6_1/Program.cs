0cb5c9beb2d2c652af7b214b94f163b1601237fe
Вахит Тухватуллин <vahta98@gmail.com>
2025-06-13 20:07:14 +0300

    Таск6.1

---
+++ Task6_1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите преложение без знаков препинания: ");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(' ');
+            int maxL = 0;
+            string maxWord = string.Empty;
+            foreach (string word in words)
+            {
+                if (maxL < word.Length)
+                {
+                    maxL = word.Length;
+                    maxWord = word;
+                }
+            }
+            Console.WriteLine("Самое длинное слово: " + maxWord);
+            Console.ReadKey();
+        }
+    }
+}
