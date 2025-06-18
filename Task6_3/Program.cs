f0558b497a289047d23022330d1421b11ff48f26
Вахит Тухватуллин <vahta98@gmail.com>
2025-06-19 01:17:06 +0300

    Таск6.3

---
+++ Task6_3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task6_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Месяц: ");
+            string month = Console.ReadLine();
+            Console.Write("Год: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Общая сумма продаж: ");
+            double summ = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Количество проданных товаров: ");
+            int items = Convert.ToInt32(Console.ReadLine());
+            double average = items > 0 ? summ / items : 0;
+            string summFormat = String.Format("{0:N2}", summ);
+            string itemsFormat = String.Format("{0:N0}", items);
+            string averageFormat = String.Format("{0:N2}", average);
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Отчёт о продажах за {month} {year} г.");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Общая сумма продаж: {summFormat} р.");
+            Console.WriteLine($"Количество проданных товаров: {itemsFormat} шт.");
+            Console.WriteLine($"Средняя стоимость товара: {averageFormat} р.");
+            Console.ReadKey();
+        }
+    }
+}
